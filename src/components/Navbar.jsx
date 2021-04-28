ad4488937df1d49d6cafbb858b36e8ef7bae9c01
ndana <nathan.dana@maxar.com>
2021-04-28 16:22:07 -0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import { Button, ListGroup, InputGroup, FormControl, Navbar as BSNavbar, Nav } from 'react-bootstrap'
+import { Navbar as BSNavbar, Nav } from 'react-bootstrap'
 
 export function Navbar({ program, city, onLogout }) {
 
 
     return (
         <BSNavbar style={{ backgroundColor: program ? program.attributes.navbarBackgroundColor : 'black' }}>
-            <BSNavbar.Brand href="/program" style={{ color: program ? program.attributes.navbarFontColor : 'black' }}>
-                <img
+            <BSNavbar.Brand className="ev-navbar-brand fn-lg d-flex align-items-center ember-view" href="/program">
+                {/* <img
                     alt=""
                     src={city.attributes.logoSmall}
                     className="d-inline-block align-top"
-                />{' '}
-            City of Golden blah taco
+                /> */}
+                <span style={{ color: program ? program.attributes.navbarFontColor : 'black' }} className="d-block d-sm-none">
+                    {program ? program.attributes.programName : ''}
+                </span>
+                <span class="d-none d-sm-block fn-lg ev-nav-font">
+                    City of Golden
+                    <strong style={{ color: program ? program.attributes.navbarFontColor : 'black' }}>
+                        <span> </span>
+                        {program ? program.attributes.programName : ''}
+                    </strong>
+                    <br />
+                    <span className="font-fine ev-nav-font">
+                        Environmental Services
+                    </span>
+                </span>
             </BSNavbar.Brand>
             <Nav className="ml-auto">
-                <Nav.Item style={{ color: 'white' }} onClick={onLogout}>Logout</Nav.Item>
+                <Nav.Item className="ev-nav-font" onClick={onLogout}>Logout</Nav.Item>
             </Nav>
         </BSNavbar>
     )
