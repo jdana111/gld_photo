28189f7626ede23fb6e6d20acade87589dcc94dc
ndana <nathan.dana@maxar.com>
2021-04-28 16:39:20 -0600

    navbar tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
-import { Navbar as BSNavbar, Nav } from 'react-bootstrap'
+import { Navbar as BSNavbar, Nav, Container, Row } from 'react-bootstrap'
+import { FaSignOutAlt } from 'react-icons/fa'
 
 export function Navbar({ program, city, onLogout }) {
 
 
     return (
         <BSNavbar style={{ backgroundColor: program ? program.attributes.navbarBackgroundColor : 'black' }}>
-            <BSNavbar.Brand className="ev-navbar-brand fn-lg d-flex align-items-center ember-view" href="/program">
-                {/* <img
-                    alt=""
-                    src={city.attributes.logoSmall}
-                    className="d-inline-block align-top"
-                /> */}
-                <span style={{ color: program ? program.attributes.navbarFontColor : 'black' }} className="d-block d-sm-none">
-                    {program ? program.attributes.programName : ''}
-                </span>
-                <span class="d-none d-sm-block fn-lg ev-nav-font">
-                    City of Golden
-                    <strong style={{ color: program ? program.attributes.navbarFontColor : 'black' }}>
-                        <span> </span>
-                        {program ? program.attributes.programName : ''}
-                    </strong>
-                    <br />
-                    <span className="font-fine ev-nav-font">
-                        Environmental Services
-                    </span>
-                </span>
-            </BSNavbar.Brand>
-            <Nav className="ml-auto">
-                <Nav.Item className="ev-nav-font" onClick={onLogout}>Logout</Nav.Item>
-            </Nav>
+            <Container>
+                <div className="navbar-container">
+                    <BSNavbar.Brand className="ev-navbar-brand fn-lg d-flex align-items-center ember-view" href="/program">
+                        {/* <img
+                            alt=""
+                            src={city.attributes.logoSmall}
+                            className="d-inline-block align-top"
+                        /> */}
+                        <span style={{ color: program ? program.attributes.navbarFontColor : 'black' }} className="d-block d-sm-none">
+                            {program ? program.attributes.programName : ''}
+                        </span>
+                        <span className="d-none d-sm-block fn-lg ev-nav-font">
+                            City of Golden
+                            <strong style={{ color: program ? program.attributes.navbarFontColor : 'black' }}>
+                                <span> </span>
+                                {program ? program.attributes.programName : ''}
+                            </strong>
+                            <br />
+                            <span className="font-fine ev-nav-font">
+                                Environmental Services
+                            </span>
+                        </span>
+                    </BSNavbar.Brand>
+                    <Nav className="ml-auto ev-navbar-nav">
+                        <Nav.Item className="ev-nav-font" onClick={onLogout}>
+                            <FaSignOutAlt/>
+                            <span className="d-none d-sm-block ml-2">
+                                Logout
+                            </span>
+                        </Nav.Item>
+                    </Nav>
+                </div>
+            </Container>
         </BSNavbar>
     )
 }
