599d07a569a1bf971de245bb8c54187ddd42ec74
John Dana <johndana@BigCorn.local>
2021-05-04 16:33:53 -0600

    logout killed and photo sizing

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import './Styling/Foundation.scss';
 import './Styling/ProgramSelect.scss';
 import './Styling/SearchTool.scss';
 import './Styling/Banner.scss';
-import './Styling/PhotoPreview.scss';
 import './Styling/NavbarEV.scss';
 
 function App() {
